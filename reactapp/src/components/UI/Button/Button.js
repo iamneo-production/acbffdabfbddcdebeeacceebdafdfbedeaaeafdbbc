de6d292a993fd0f7466bd6b40abbaf7e348490a6
Iamneo <techadmin@iamneo.ai>
2023-08-03 07:36:32 +0000

    Backup Commit

--- reactapp/src/components/UI/Button/Button.js
+++ reactapp/src/components/UI/Button/Button.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 
-const Button = (ButtonProps) => {
-  return (<button onClick = {ButtonProps.onClick}>{ButtonProps.children}</button>);
-}
-
+const Button = ({ onClick, children }) => {
+  return (
+    <button type="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+};
 
 export default Button;
